2405cd43091d5697eeab5c45ad4fc7e41d6d32ec
Temirlan <ltemirlan2003@gmail.com>
2024-03-21 22:32:41 +0600

    init: formulas(nextjs-app)

---
+++ apps/frontend/formulas-e2e/cypress.config.ts
@@ -0,0 +1,14 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: { default: 'nx run formulas:start' },
+      ciWebServerCommand: 'nx run formulas:serve-static',
+    }),
+    baseUrl: 'http://localhost:3000',
+  },
+});
