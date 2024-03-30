7ad3905c6cbed359bad9a7aa40af982478d1b9fe
Temirlan <ltemirlan2003@gmail.com>
2024-03-30 19:04:33 +0600

    refactor: formulas/auth/data-access services

--- libs/formulas/auth/data-access/src/index.ts
+++ libs/formulas/auth/data-access/src/index.ts
@@ -1,4 +1,6 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
-export * from './lib/services/login.service';
+import { loginService } from './lib/services/login.service';
+
 export * from './lib/conrollers/auth.conroller';
+export const loginWithCredentials = loginService().loginWithCredentials;
--- libs/formulas/auth/data-access/src/lib/services/login.service.ts
+++ libs/formulas/auth/data-access/src/lib/services/login.service.ts
@@ -1,18 +1,19 @@
 import { apifetch } from '@/libs/core/http/src';
 import { UserData } from '@/libs/formulas/utils/types';
 
-export const loginWithCredentials = async ({
-  username,
-  password,
-}: {
-  username: string;
-  password: string;
-}) => {
-  return await apifetch<UserData>('auth/login', {
-    method: 'POST',
-    body: JSON.stringify({ username, password }),
-    headers: {
-      'Content-Type': 'application/json',
+export const loginService = () => {
+  return {
+    async loginWithCredentials({
+      username,
+      password,
+    }: Pick<UserData, 'username'> & { password: string }) {
+      return await apifetch<UserData>('auth/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     },
-  });
+  };
 };
