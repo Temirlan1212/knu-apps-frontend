8856c7b936511fabee5a91bd82436b2024b5d027
Temirlan <ltemirlan2003@gmail.com>
2024-04-03 14:16:09 +0600

    libs/formulas/blog add: blog-cover-image unsplash random image

--- libs/formulas/blog/src/lib/components/blog-cover-image.tsx
+++ libs/formulas/blog/src/lib/components/blog-cover-image.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 
 export function BlogCoverImage(props: UnsplashListProps) {
   const [url, setUrl] = useState(
-    'https://plus.unsplash.com/premium_photo-1667239301911-bf995dffd979?q=80&w=3112&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    'https://source.unsplash.com/random/1200×250?theme,math,physics,backround'
   );
   return (
     <div className="relative">
