12ad6ac31ff6adc812353f1cfe59f009abdf9a41
Temirlan <ltemirlan2003@gmail.com>
2024-04-04 01:19:33 +0600

    update: imports

--- libs/formulas/blog/src/lib/data-access/index.ts
+++ libs/formulas/blog/src/lib/data-access/index.ts
@@ -1,16 +1,8 @@
 import { useBlogState, useBlogCuState } from './store/blog.store';
-import {
-  useBlogDocumentState,
-  BlogUpdateStatusText,
-} from './store/blog-document.store';
 import { blogController as blogControllerLocal } from './conrollers/blog-crud.conroller';
 export * from './conrollers/blocknote-document-crud.conroller';
+export * from './store/blog-document.store';
+
 const blogController = blogControllerLocal();
 
-export {
-  blogController,
-  useBlogState,
-  useBlogCuState,
-  BlogUpdateStatusText,
-  useBlogDocumentState,
-};
+export { blogController, useBlogState, useBlogCuState };
